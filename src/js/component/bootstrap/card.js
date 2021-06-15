fa8adcd90968f19b9b939a2efb24a46460a7cdcd
Manuel Filgueira Marin <manuelfilgueira@gmail.com>
2021-06-15 18:11:04 +0000

    card actualizadas

--- src/js/component/bootstrap/card.js
+++ src/js/component/bootstrap/card.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ButtonSeeDetail } from "../button-see-detail";
 import { ButtonToggleFavorite } from "../button-toggle-favorite";
-import starWars400x200 from "../../../img/star-wars-400x200.jpg";
+import starWars2 from "../../../img/start-wars-2.jpg";
 import "../../../styles/index.scss";
 
 export const Card = props => {
@@ -24,7 +24,7 @@ export const Card = props => {
 	return (
 		<div className="col-3">
 			<div className="card card-block">
-				<img src={starWars400x200} className="card-img-top" alt="star wars image" />
+				<img src={starWars2} className="card-img-top" alt="star wars image" />
 				<div className="card-body d-flex flex-column">
 					<h5 className="card-title">{props.name}</h5>
 					{mapDetails}
